f169d5f23d11fe288d016d1082df2d33a49d7875
fataranto <fataranto@gmail.com>
2022-06-15 01:08:38 +0200

    ex 4 palíndromo y ruta dinámica ok

--- server4.js
+++ server4.js
@@ -2,15 +2,21 @@ const express = require("express");
 const verPalindrome = require("./utils/palindrome/index")
 const app = express();
 
-app.get("/check", (req, res) => {
-    const palindrome = req.query.palindrome;
-    res.status(200).send("<h1>Bienvenido a server 2</h1>");
-    
-})
 
-app.use((req, res) => {
-    res.status(400).send(`<p style='font-size:200px'>ERROR ${res.status(404).statusCode}</p>`);
+app.get('/check/:pal', function (req, res) {
+    const checkPal = verPalindrome.palindrome(req.params.pal);
+    //res.send('El palíndromo que quieres verificar es ' + req.params.pal);
+    if(checkPal) {
+    res.send(`<h2><strong>"${req.params.pal}"</strong> es un palíndromo</h2>`);
+}else {
+    res.send(`<h2><strong>"${req.params.pal}"</strong> NO es un palíndromo</h2>`);
+}
+});
+
+
+ app.use((req, res) => {
+    res.status(400).send(`<p>para verificar un palídromo debes utilizar el siguiente formato de ruta:<br>localhost:3000/check/tupalíndromo</p>`);
     //console.log(res.status(404).statusMessage);
-  })
+  }) 
 
 app.listen(3000);
